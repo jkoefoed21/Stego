972e41332434c7176ef181f9c1983b5a8b9f44a2
Jack Koefoed <jackkoefoed@hotmail.com>
2017-05-02 20:39:59 -0700

    Project Start

---
+++ Stego Stuff/StegoHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stego_Stuff
+{
+    class StegoHandler
+    {
+        public static void Main(String[] args)
+        {
+
+        }
+        
+        public static String getPassword()
+        {
+            return null;
+        }
+
+        public static byte[] getKeyFromPassword(string password)
+        {
+            return null;
+        }
+
+    }
+}
